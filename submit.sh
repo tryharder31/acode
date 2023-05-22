2b89d6e333bb19a509d2348da880fdcc5298c28f
tryharder31 <omer@justa.app>
2023-05-22 18:52:47 +0200

    a

--- submit.sh
+++ submit.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=<Your E-mail> # for example uname@hi.is
-#SBATCH --partition=48cpu_192mem  # request node from a specific partition
+#SBATCH --mail-user=korat@hi.is # for example uname@hi.is
+#SBATCH --partition=gpu-2xA100  # request node from a specific partition
 #SBATCH --nodes=2                 # number of nodes
 #SBATCH --ntasks-per-node=48      # 48 cores per node (96 in total)
 #SBATCH --mem-per-cpu=3900        # MB RAM per cpu core
-#SBATCH --time=0-04:00:00         # run for 4 hours maximum (DD-HH:MM:SS)
 #SBATCH --hint=nomultithread      # Suppress multithread
 #SBATCH --output=slurm_job_output.log
 #SBATCH --error=slurm_job_errors.log   # Logs if job crashes
