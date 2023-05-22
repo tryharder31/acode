41fdd0e187f2b13b0c1af8bb5f339bb2aee5b93a
tryharder31 <omer@justa.app>
2023-05-22 19:38:58 +0200

    awd

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+#git commit -m "a" && git add . && git push
 import numpy as np
 import tensorflow as tf
-#git commit -m "a" && git add . && git push
-import tensorflow as tf
 
+output_file = "output.txt"
+f = open(output_file, "w")
 # Check if GPU is available
 if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()),file=f)
 else:
-    print("Please install GPU version of TF")
+    print("Please install GPU version of TF",file=f)
 
 # Create two matrices
 matrix1 = tf.constant([[3., 3.]])
@@ -16,4 +17,4 @@ matrix2 = tf.constant([[2.],[2.]])
 # Perform matrix multiplication
 product = tf.matmul(matrix1, matrix2)
 
-print(product)
+print(product,file=f)
