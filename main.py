9bb3050f1add5c5be3cc665e00c6f20c2ae33c89
tryharder31 <omer@justa.app>
2023-05-22 18:24:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+with open('test.dat') as f:
+    with open('output.dat', 'w') as f2:
+        for line in f:
+            print(line,file=f2)
+
+quit()
+
+class ExIsTextEngine(TextEngine):
+        name = 'ex-is'
+        embedding_model = "jonfd/convbert-base-igc-is"
+        train_sets = ['train']
+        dev_sets = []
+        test_sets = []
+
+        labels = ['UNLABELLED']
+
+        input_dir = 'data/example_corpus'
+
+        def iter_files(self, dsets=None, sents_limit=None):
+            if dsets is None:
+                dsets = self.dsets
+            for dset in dsets:
+                for filename in listdir(join(self.input_dir, dset)):
+                    with open(join(self.input_dir, dset, filename)) as f:
+                        for line in f:
+                            sentence = line
+                            label = 'UNLABELLED'
+
+                            yield sentence, Config(label=label, time=None)
+
+
+class AlthingiUpptokurTextEngine(TextEngine):
+    name = 'alth-upp'
+    embedding_model = "jonfd/convbert-base-igc-is"
+    train_sets = ['train']
+    dev_sets = ['dev']
+    test_sets = ['eval']
+
+    labels = ['UNLABELLED']
+
+    input_dir = 'data/example_corpus'
+
+    def iter_files(self, dsets=None, sents_limit=None):
+        if dsets is None:
+            dsets = self.dsets
+        for dset in dsets:
+            for filename in listdir(join(self.input_dir, dset)):
+                with open(join(self.input_dir, dset, filename)) as f:
+                    for line in f:
+                        sentence = line
+                        label = 'UNLABELLED'
+
+                        yield sentence, Config(label=label, time=None)
+
