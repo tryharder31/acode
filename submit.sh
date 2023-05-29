8ea131fc78723349216570aa99473f8a2dc32cdd
tryharder31 <omer@justa.app>
2023-05-29 10:11:26 +0200

    a

--- submit.sh
+++ submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL
-#SBATCH --partition=gpu-2xA100 # request node from a specific partition
-#SBATCH --nodes=1 # number of nodes
-#SBATCH --ntasks-per-node=64
+#SBATCH --partition=48cpu_192mem  # request node from a specific partition
+#SBATCH --nodes=2                 # number of nodes
+#SBATCH --ntasks-per-node=48      # 48 cores per node (96 in total)
 #SBATCH --output=slurm_job_output.log
-#SBATCH --error=slurm_job_errors.log # Logs if job crashes
+#SBATCH --error=slurm_job_errors.log   # Logs if job crashes
 
 . ~/.program_env_bash
 
